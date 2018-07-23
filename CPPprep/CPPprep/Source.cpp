634eac457876c40c369ab0c476ae33b8dd2d98ca
mitchellrowley <mitchellkrowley1984@gmail.com>
2018-07-23 13:29:20 -0700

    Fist entry
    This is the first entry into this repo

---
+++ CPPprep/CPPprep/Source.cpp
@@ -0,0 +1,12 @@
+#//include <iostream>
+
+#include <stdio.h>
+// below is just a function. This shows that C++ does not HAVE to be OOP
+int main()
+{
+	printf("Hi\r\n");
+	int a = 2;
+	double double_num = 2.0;
+	//std::cout << "Hi" << std::endl;
+	return 0;
+}
