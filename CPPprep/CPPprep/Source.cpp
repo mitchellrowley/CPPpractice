e4d4e5c1afa65fc1d291c86551e0628c71813886
mitchellrowley <mitchellkrowley1984@gmail.com>
2018-07-28 13:48:51 -0700

    New Branch
    Using this branch to dispose of old code

--- CPPprep/CPPprep/Source.cpp
+++ CPPprep/CPPprep/Source.cpp
@@ -1,12 +1,31 @@
-#//include <iostream>
+//#include <iostream>
 
-#include <stdio.h>
+//#include <stdio.h>
 // below is just a function. This shows that C++ does not HAVE to be OOP
+
+
+
+extern int g_variable;
+
 int main()
 {
+	int wer = foo();
 	printf("Hi\r\n");
-	int a = 2;
-	double double_num = 2.0;
+	//int a = 2;
+	//double double_num = 2.0;
+	
+	double final_gpa = 0;
+	{
+		double gpa = 3.0;
+		double modded_gpa = gpa * 1.2;
+		final_gpa = modded_gpa;
+	}
+
+	double gpa = 0;
+
+	g_variable = 5;
+	//double cout = 4;
+	cout << "the value of g_variable = " << g_variable;
 	//std::cout << "Hi" << std::endl;
 	return 0;
 }
