12d3a3bf9e240548535ea194a298bd78b735aab0
djulzz <julien.esposito@gmail.com>
2018-07-28 13:27:08 -0700

    This Commit is About Arrays and Why They Are Convenients
    - Note: in void W2_Without_arrays(), we are declaring variables in block, coz it's easier.

---
+++ CPPprep/CPPprep/W2_Lesson_01_Arrays.cpp
@@ -0,0 +1,11 @@
+// 1 - how to cr
+
+// First example: when not using arrays
+void W2_Without_arrays()
+{
+	double hr_1, hr_2, hr_3;
+}
+void W2_Lesson_01_Arrays()
+{
+
+}
---
+++ CPPprep/CPPprep/main.cpp
@@ -0,0 +1,15 @@
+
+
+#include <cstdio> // writting to the console
+#include <string> // std::string 
+#include <iostream> // for cout 
+
+
+void W2_Lesson_01_Arrays();
+
+int main()
+{
+	W2_Lesson_01_Arrays();
+	//std::cout << "Hello\r\n" << std::endl;;
+	return 0;
+}
